8c3e9cb56c2d1020b903582a58207739ecb19832
ersandeepahirwar <codeysandeep@gmail.com>
2022-10-18 14:29:55 +0530

    Initialized the Project

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon</title>
+        <meta
+          name="description"
+          content="Amazon 2.0 built with Next.js by Codey Sandeep"
+        />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+    </div>
+  );
+}
